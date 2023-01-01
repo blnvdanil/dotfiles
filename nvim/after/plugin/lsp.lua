b0c0bf610c1623cdf38686a171c18ab2002d77c8
Danil B <blnvdanil@gmail.com>
2023-01-01 18:21:03 +0300

    i3

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -5,11 +5,25 @@ if (not status) then
   return
 end
 
+lsp.set_preferences({
+  suggest_lsp_servers = true,
+  setup_servers_on_start = true,
+  set_lsp_keymaps = true,
+  configure_diagnostics = true,
+  cmp_capabilities = true,
+  manage_nvim_cmp = true,
+  call_servers = 'local',
+  sign_icons = {
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = ''
+  }
+})
+
 lsp.ensure_installed({
   "tsserver",
   "eslint",
   "sumneko_lua",
 })
-
-lsp.preset("recommended")
 lsp.setup()
